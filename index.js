c851bb52e0d0a7b92572cce360b6241ca6f2ca2f
Allison Lu <allison.lu@gatech.edu>
2022-04-12 12:17:20 -0400

    Link pages

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const { json } = require("express/lib/response");
 
 const app = express();
 app.use(express.static("public"));
 
+app.get("/index", function (req, res) {
+    res.sendFile(__dirname + "/public/" + "index.html");
+})
+
+app.get("/about", function (req, res) {
+    res.sendFile(__dirname + "/public/" + "about.html");
+})
+
+app.get("/commissions", function (req, res) {
+    res.sendFile(__dirname + "/public/" + "commissions.html");
+})
+
+app.get("/shop", function (req, res) {
+    res.sendFile(__dirname + "/public/" + "shop.html");
+})
 
 app.listen(3000, function () {
     console.log("Listening on port 3000...");
